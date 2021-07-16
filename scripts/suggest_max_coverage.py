4d463bcf37197910bc6b29e39c8f38d08438fbbc
kcleal <clealk@cardiff.ac.uk>
2021-07-16 13:32:00 +0100

    Fixed args

--- scripts/suggest_max_coverage.py
+++ scripts/suggest_max_coverage.py
@@ -8,12 +8,10 @@ from dysgu.coverage import index_stats
 @click.argument('alignment_file', required=True, type=click.Path(exists=False))
 @click.option("-y", "--y", help="Max-cov is estimated as {mean-coverage} * y", required=False, type=float,
               show_default=True, default=6)
-@click.option("--reference", help="Reference file for opening cram file", required=False, type=click.Path(),
-              show_default=False, default=None)
-def suggest_max_coverage(alignment_file, y, reference):
+def suggest_max_coverage(alignment_file, y):
     """Estimate a max-coverage value for use with dysgu. Mean genome coverage is estimated from the index file, so
     will only be useful for whole-genome alignment files"""
-    f = pysam.AlignmentFile(alignment_file, reference_filename=reference)
+    f = pysam.AlignmentFile(alignment_file)
     cov, read_length = index_stats(f)
     cov = round(cov, 2)
     read_length = round(read_length, 1)
